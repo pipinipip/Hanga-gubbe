a3fba5c677f3a4f1b0606faf77819b579abeb9d9
pipinipip <07593.ypo@gmail.com>
2020-01-23 11:16:01 +0100

    2020-01-23 försöker få knappar att fungera.

--- src/me/pipinipip/Main.java
+++ src/me/pipinipip/Main.java
@@ -6,8 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+      int mot = JOptionPane.showOptionDialog()null,"mot dator eller spelare",  JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+
+
+
+
+
+
+        /*
         final String ord = JOptionPane.showInputDialog("skriv ett ord");
         System.out.println(ord);
+        // Ord input system.
 
         int Count = 0;
 
@@ -17,5 +27,9 @@ public class Main {
                 Count++;
         }
         System.out.println(Count);
+        // Räknar hur många bokstäver som finns i ett ord.
+
+*/
     }
+
 }
