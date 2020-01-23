c73c486ec1b14b1c8ba8d873879f9c6a56a400b3
pipinipip <07593.ypo@gmail.com>
2020-01-23 10:33:17 +0100

    2020-01-23 ord input och system för att räkna mängden bokstäver

--- src/me/pipinipip/Main.java
+++ src/me/pipinipip/Main.java
@@ -1,9 +1,21 @@
 package me.pipinipip;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        final String ord = JOptionPane.showInputDialog("skriv ett ord");
+        System.out.println(ord);
 
+        int Count = 0;
+
+
+        for( int i = 0; i < ord.length( ); i++ )
+        {
+                Count++;
+        }
+        System.out.println(Count);
     }
 }
