e668d7637266df548489bd8350230c855b9ea2e7
pipinipip <07593.ypo@gmail.com>
2020-01-24 11:05:59 +0100

    2020-01-23 array

--- src/me/pipinipip/Main.java
+++ src/me/pipinipip/Main.java
@@ -6,30 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-      int mot = JOptionPane.showOptionDialog()null,"mot dator eller spelare",  JOptionPane.DEFAULT_OPTION,
-                JOptionPane.QUESTION_MESSAGE);
+     // String mot = JOptionPane.showInputDialog("skriv dator eller spelare för att välja vad du vill spela mot");
 
-
-
-
-
-
-        /*
         final String ord = JOptionPane.showInputDialog("skriv ett ord");
         System.out.println(ord);
         // Ord input system.
 
-        int Count = 0;
+        int ordlängd = 0;
 
 
         for( int i = 0; i < ord.length( ); i++ )
         {
-                Count++;
+                ordlängd++;
         }
-        System.out.println(Count);
+        System.out.println(ordlängd);
         // Räknar hur många bokstäver som finns i ett ord.
 
-*/
+        char[] bokstäver = new char[ord.length()];
+        for( int i = 0; i < ord.length( ); i++ )
+        {
+            bokstäver[i] = ord.charAt(i);
+        }
+
+        System.out.println(bokstäver);
+
+
     }
 
 }
+
